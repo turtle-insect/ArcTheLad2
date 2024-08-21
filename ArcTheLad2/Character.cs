12843e8e3ab0595f70cdb2675f939dacbab78ed7
turtle-insect <turtle.insect@gmail.com>
2024-08-21 17:10:14 +0900

    first commit

---
+++ ArcTheLad2/Character.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcTheLad2
+{
+	internal class Character
+	{
+		private readonly uint mAddress;
+
+		public Character(uint address)
+		{
+			mAddress = address;
+		}
+
+		public uint Lv
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 0, 2);
+			set => Util.WriteNumber(mAddress + 0, 2, 1, 1000, value);
+		}
+
+		public uint MaxHP
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 2, 2);
+			set => Util.WriteNumber(mAddress + 2, 2, 1, 9999, value);
+		}
+
+		public uint HP
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 4, 2);
+			set => Util.WriteNumber(mAddress + 4, 2, 0, 9999, value);
+		}
+
+		public uint MaxMP
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 6, 2);
+			set => Util.WriteNumber(mAddress + 6, 2, 1, 9999, value);
+		}
+
+		public uint MP
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 8, 2);
+			set => Util.WriteNumber(mAddress + 8, 2, 0, 9999, value);
+		}
+
+		public uint Attack
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 10, 2);
+			set => Util.WriteNumber(mAddress + 10, 2, 1, 2499, value);
+		}
+
+		public uint Magic
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 12, 2);
+			set => Util.WriteNumber(mAddress + 12, 2, 1, 2499, value);
+		}
+
+		public uint Defense
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 14, 2);
+			set => Util.WriteNumber(mAddress + 14, 2, 1, 2499, value);
+		}
+
+		public uint Speed
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 16, 1);
+			set => Util.WriteNumber(mAddress + 16, 1, 1, 255, value);
+		}
+
+		public uint ID
+		{
+			get => SaveData.Instance().ReadNumber(mAddress + 17, 1);
+			set => SaveData.Instance().WriteNumber(mAddress + 17, 1, value);
+		}
+	}
+}
---
+++ ArcTheLad2/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ArcTheLad2
+{
+	internal class ViewModel
+	{
+		public Info Info { get; } = Info.Instance();
+
+		public ICommand OpenFileCommand { get; }
+		public ICommand SaveFileCommand { get; }
+
+		public ObservableCollection<Character> Party { get; } = new ObservableCollection<Character>();
+
+		public ViewModel()
+		{
+			OpenFileCommand = new ActionCommand(OpenFile);
+			SaveFileCommand = new ActionCommand(SaveFile);
+		}
+
+		private void Initialize()
+		{
+			Party.Clear();
+
+			for (uint index = 0; index < 32; index++)
+			{
+				Party.Add(new Character(0x2624 + index * 260));
+			}
+		}
+
+		private void OpenFile(Object? param)
+		{
+			var dlg = new OpenFileDialog();
+			dlg.DefaultExt = "mcd";
+			if (dlg.ShowDialog() == false) return;
+
+			if (SaveData.Instance().Open(dlg.FileName))
+			{
+				Initialize();
+			}
+		}
+
+		private void SaveFile(Object? param)
+		{
+			SaveData.Instance().Save();
+		}
+	}
+}
